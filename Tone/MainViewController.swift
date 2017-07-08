771f6592ab8e52ab2199b4a4c76fd309ceab71b5
caziz <cmaisme@gmail.com>
2017-07-08 11:19:03 -0700

    initial commit

---
+++ Tone/MainViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MainViewController.swift
+//  Tone
+//
+//  Created by Christopher Aziz on 7/8/17.
+//  Copyright © 2017 Christopher Aziz. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBAction func toggleRecording(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
