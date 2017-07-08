b012230ec3a09e37313506e52b2bcd8946657a3d
caziz <cmaisme@gmail.com>
2017-07-08 11:36:55 -0700

    constants

---
+++ Tone/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  Tone
+//
+//  Created by Christopher Aziz on 7/8/17.
+//  Copyright © 2017 Christopher Aziz. All rights reserved.
+//
+
+
+struct Constants {
+    
+    struct ToneAnalyzer {
+        let username = "ac0aab1c-d91b-4ed5-9522-d85064e5bc27"
+        let password = "Q7oVfvPqt0ZT"
+        let version = "2017-06-07"
+    }
+    
+    struct SpeechToText {
+        let username = "2f9c5e3a-d8b8-4500-b217-ad9068d9d2e8"
+        let password = "dBZd75IHEZOG"
+    }
+    
+}
