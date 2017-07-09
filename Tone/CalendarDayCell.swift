f5010122f48637341cc0e31ea51b39dcf506e072
Specialist17 <arocho.fernando.13@gmail.com>
2017-07-08 19:56:01 -0700

    finised calendar UI 2

--- Tone/CalendarDayCell.swift
+++ Tone/CalendarDayCell.swift
@@ -7,7 +7,11 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class CalendarDayCell: JTAppleCell {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIView!
+    
 }
